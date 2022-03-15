cf8c262836cd60a4afb641a472cb0e5e74d05f9f
Carlos Vasquez <virahondacarlos@gmail.com>
2022-03-15 09:14:57 -0500

    se agregan servicios para el algolia

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,36 +1,67 @@
 "use strict";
 const DB = require("../config/database");
-//Models
+const algoliasearch = require("algoliasearch/lite");
+
+/* import algoliasearch from "algoliasearch/lite"; */
+// API keys below contain actual values tied to your Algolia account
+const client = algoliasearch("I2JCDIL0V7", "097b8eda827e3f6e958f712f584c0e8c");
+const index = client.initIndex("poc_mages");
 
 const controller = {
-    getPrueba: async (req, res) => {
-        try {
-            const response = await DB.query(`SELECT * FROM apiprueba`);
-            res.status(200).json({
-                code: 200,
-                status: "SUCCESS",
-                message: "OK",
-                response: response.rows,
-            });
-        } catch (error) {
-            res.status(500).send("error");
-        }
-    },
-    getPruebaId: async (req, res) => {
-        try {
-            const response = await DB.query(
-                `SELECT * FROM apiprueba WHERE id = ${req.params.id}`
-            );
-            res.status(200).json({
-                code: 200,
-                status: "SUCCESS",
-                message: "OK",
-                response: response.rows[0],
-            });
-        } catch (error) {
-            res.status(500).send("error");
-        }
-    },
+  getAlgolia: async (req, res) => {
+    try {
+      const response = await index.search("");
+      res.status(200).json({
+        code: 200,
+        status: "SUCCESS",
+        message: "OK",
+        response: response,
+      });
+    } catch (error) {
+      res.status(500).send("error");
+    }
+  },
+  getAlgoliaId: async (req, res) => {
+    try {
+      const response = await index.findObject((hit) => hit.id == req.params.id);
+      res.status(200).json({
+        code: 200,
+        status: "SUCCESS",
+        message: "OK",
+        response: response,
+      });
+    } catch (error) {
+      res.status(500).send("error");
+    }
+  },
+  getServer: async (req, res) => {
+    try {
+      const response = await DB.query(`SELECT * FROM apiprueba`);
+      res.status(200).json({
+        code: 200,
+        status: "SUCCESS",
+        message: "OK",
+        response: response.rows,
+      });
+    } catch (error) {
+      res.status(500).send("error");
+    }
+  },
+  getServerId: async (req, res) => {
+    try {
+      const response = await DB.query(
+        `SELECT * FROM apiprueba WHERE id = ${req.params.id}`
+      );
+      res.status(200).json({
+        code: 200,
+        status: "SUCCESS",
+        message: "OK",
+        response: response.rows[0],
+      });
+    } catch (error) {
+      res.status(500).send("error");
+    }
+  },
 };
 
 module.exports = controller;
